997ff47eb8c65bf6ebe10ae883c980e352ea6a72
juanestrella2098 <131753015+juanestrella2098@users.noreply.github.com>
2025-03-11 00:10:33 +0100

    Arreglado variable de entorno

--- src/main.ts
+++ src/main.ts
@@ -1,7 +1,4 @@
 /* eslint-disable prettier/prettier */
-import * as dotenv from 'dotenv';
-dotenv.config();
-
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 import { ValidationPipe } from '@nestjs/common';
